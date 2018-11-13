56a16b3b2e8a9801e7f87488bf5d2b240a209f31
Berk Ergün <hasan.berk.ergun@hotmail.com>
2018-11-14 00:31:27 +0300

    File Operation Completed

---
+++ CharecterReplace/FileOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharecterReplace
+{
+    public class FileOperation //Sınıf İçindeki Tüm Elemanları Static Yapmak İstersen Etiketle
+    {
+        public static string ReadFromFile(string _path,char[] oldValue,char[] newValue)
+        {
+            string path = _path;
+            // string text = File.ReadAllText(path).Replace(oldValue, newValue);
+            string text = File.ReadAllText(path);
+            for (int i = 0; i < oldValue.Length; i++) //Or New Value it doesnt matter
+                text.Replace(oldValue[i],newValue[i]);
+            File.WriteAllText(path, text);
+            return text;
+        }
+    }
+}
