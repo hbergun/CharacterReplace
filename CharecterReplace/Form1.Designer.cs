36a0b40fd5c186f4ac03d426393566a49769a75e
Berk Ergün <hasan.berk.ergun@hotmail.com>
2018-11-13 23:50:12 +0300

    First Commit

---
+++ CharecterReplace/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CharecterReplace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ofdDosyaAc = new System.Windows.Forms.OpenFileDialog();
+            this.btnFindPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFindPath
+            // 
+            this.btnFindPath.Location = new System.Drawing.Point(32, 32);
+            this.btnFindPath.Name = "btnFindPath";
+            this.btnFindPath.Size = new System.Drawing.Size(118, 26);
+            this.btnFindPath.TabIndex = 0;
+            this.btnFindPath.Text = "Find Path";
+            this.btnFindPath.UseVisualStyleBackColor = true;
+            this.btnFindPath.Click += new System.EventHandler(this.btnFindPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFindPath);
+            this.Name = "Form1";
+            this.Text = "frmMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ofdDosyaAc;
+        private System.Windows.Forms.Button btnFindPath;
+    }
+}
+
---
+++ CharecterReplace/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharecterReplace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindPath_Click(object sender, EventArgs e)
+        {
+            //ofdDosyaAc.InitialDirectory = "C:\\Users\\HasanBerkErgun\\Desktop"; // 1.Yol
+            ofdDosyaAc.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //SpecialFolder'lar İçin
+            ofdDosyaAc.Filter = "Text Dosyası |*.txt"; //ofd'nin propertisinden de yapılabilirdi.
+            ofdDosyaAc.RestoreDirectory = true; //En Son Bakılan Path'i Aç Defualt False'dur
+            ofdDosyaAc.Title = "Please Select Your Text File";
+            //ofdDosyaAc.ShowDialog();
+
+            if (ofdDosyaAc.ShowDialog() == DialogResult.OK)
+            {
+                string FilePath = ofdDosyaAc.FileName;
+                MessageBox.Show(FilePath);
+                string FileName = ofdDosyaAc.SafeFileName;
+                MessageBox.Show(FileName);
+            }
+
+
+        }
+    }
+}
